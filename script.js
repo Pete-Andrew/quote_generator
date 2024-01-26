229775fbff88cc2f9d05d78f4685234891fbd8d6
Peter Andrew <peter.a.andrew@gmail.com>
2024-01-26 16:58:28 +0000

    added a back button to check previous quotes

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 let apiQuotes = [];
+let storedQuotesArray = [];
+let storedQuoteIndexNumber = 0;
 
 const quoteContainer = document.getElementById("quote-container");
 const quoteText = document.getElementById("quote-text");
 const authorText = document.getElementById("author");
 const twitterButton = document.getElementById("twitter");
 const newQuoteButton = document.getElementById("new-quote");
+const previousQuoteButton = document.getElementById("previous-quote");
 
 // Get Quotes from API
 
@@ -29,9 +32,20 @@ function newQuote() {
     //.textContent will allow us to pass in a string that is then shown in that element e.g. the Author span in the Quote-author div
     //quote.author and quote.tex breaks sends the relevant part of the authorText object to the relevant div
     quoteText.textContent = quote.text; 
-    
-    console.log(quote);
+    pushToLocalStorage(quote);    
 }
+
+function pushToLocalStorage(quote) {
+
+    //push to global array
+    storedQuotesArray.push(quote)
+    //push array to local storage
+    let storedQuotes = JSON.stringify(storedQuotesArray);
+    localStorage.setItem('storedQuote', storedQuotes);      
+    console.log("item pushed")
+    console.log("quote = " + storedQuotes)
+}
+// pushToLocalStorage();
 
 
 // asynchronous fetch request within a try/catch statement
@@ -53,5 +67,33 @@ async function getQuotes() {
     }
 }
 
+function previousQuote() { 
+    // Retrieve the stored quote from local storage
+    let calledQuote = localStorage.getItem('storedQuote');
+    console.log("calledQuote = " + calledQuote);
+    //turn it from an object into JSON object so the information can then be called. 
+    let calledQuoteIndex = JSON.parse(calledQuote)
+
+    console.log("stored quote = " + calledQuoteIndex[0].text);
+    console.log("stored quote = " + calledQuoteIndex[0].author);
+    
+    quoteText.textContent = calledQuoteIndex[0].text;
+    authorText.textContent = calledQuoteIndex[0].author;
+}
+
+//tweet quote 
+function tweetQuote() {
+//using backtick
+//tweet?text - is equivalent to query parameter = text, using the back ticks alows us to pass in a variable
+const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+// "_blank", allows the window to open in a new tab
+window.open(twitterUrl, "_blank")
+}
+
+newQuoteButton.addEventListener("click", newQuote);
+twitterButton.addEventListener("click", tweetQuote);
+previousQuoteButton.addEventListener("click", previousQuote);
+
+//on load
 getQuotes();
 
