21702abca3aacdd16272ac1caa2e481a3ce56429
Peter Andrew <peter.a.andrew@gmail.com>
2024-01-28 22:05:17 +0000

    test push

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let apiQuotes = [];
 let storedQuotesArray = [];
+let localJsonQuotes = [];
 
-//starts at 1 as quotes are zero indexed
+//quotes are zero indexed
 let storedQuoteIndexNumber = 0;
 let totalNumberOfQuotes = 0;
 
@@ -14,20 +15,33 @@ const cycleBackButton = document.getElementById("cycle-back");
 const cycleForwardButton = document.getElementById("cycle-forward");
 const arrayIndexNumber = document.getElementById("array-index");
 const totalQuoteDiv = document.getElementById("total-quote-number");
+const loader = document.getElementById("loaderID");
+
+// show loader
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+// hide loader
+function loadingComplete() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
 
 // Get Quotes from API
-
 //show new quote 
 function newQuote() {
-
+    loading();
+    //add as section that if the loading takes too long to use the local 'quotes.js' JSON instead
     
+
     //to pick a random quote from the API using math random
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     //Check if author field is blank and replace it with 'unknown'
-    if(!quote.author) {
+    if (!quote.author) {
         authorText.textContent = "Unknown"
     } else {
-        authorText.textContent = quote.author; 
+        authorText.textContent = quote.author;
     }
 
     // change the font size for long quotes.
@@ -39,37 +53,37 @@ function newQuote() {
     }
     //.textContent will allow us to pass in a string that is then shown in that element e.g. the Author span in the Quote-author div
     //quote.author and quote.tex breaks sends the relevant part of the authorText object to the relevant div
-    quoteText.textContent = quote.text; 
-    pushToLocalStorage(quote);   
-    
+    quoteText.textContent = quote.text;
+    pushToLocalStorage(quote);
+
     totalNumberOfQuotes++;
     //pushes the value of totalNumberOfQuotes to the relevant div
     totalQuoteDiv.textContent = totalNumberOfQuotes;
     //each time new quote is pushed it increases total number of quotes by one
-     
+
     console.log(totalNumberOfQuotes);
-    
-    //
+
+    //this line sets the current quotes number to the maximum when new quote is generated. The 'quoteNumber' function doesn't need to be called here. 
     arrayIndexNumber.textContent = totalNumberOfQuotes;
-    
+    //set quote and hide loader
+    loadingComplete();
 }
 
 function pushToLocalStorage(quote) {
-
     //push to global array
     storedQuotesArray.push(quote)
     //push array to local storage
     let storedQuotes = JSON.stringify(storedQuotesArray);
-    localStorage.setItem('storedQuote', storedQuotes);      
+    localStorage.setItem('storedQuote', storedQuotes);
     console.log("item pushed")
     console.log("quote = " + storedQuotes)
 }
 // pushToLocalStorage();
 
-
 // asynchronous fetch request within a try/catch statement
 // asynchronous functions can be run at any time independently and won't stop the browser loading th page. 
 async function getQuotes() {
+    loading();
     const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json"
     // try allows us to attempt to complete a fetch request from the API, but if it doesn't work we can catch the error info and do something with it.
 
@@ -80,18 +94,18 @@ async function getQuotes() {
         // can pull a single quote using array index e.g.
         //console.log(apiQuotes[12]);
         newQuote()
-        
+
     } catch (error) {
         //Catch error here, might trigger and alert or console log etc. 
     }
 }
 
 //this function has an added check added, without this the script was throwing 'Uncaught TypeError: Cannot read properties of undefined (reading 'text')'
-function previousQuote() { 
+function previousQuote() {
     // Retrieve the stored quote from local storage
     let calledQuote = localStorage.getItem('storedQuote');
     // console.log("calledQuote = " + calledQuote);
-    
+
     // Check if there are stored quotes
     if (calledQuote) {
         // Parse the stored quotes
@@ -102,7 +116,7 @@ function previousQuote() {
             // Decrement the index to get the previous quote
             storedQuoteIndexNumber--;
 
-            
+
             // Check if the index is within bounds
             if (storedQuoteIndexNumber < 0) {
                 storedQuoteIndexNumber = calledQuoteIndex.length - 1;
@@ -116,11 +130,11 @@ function previousQuote() {
     }
 }
 
-function cycleForward() { 
+function cycleForward() {
     // Retrieve the stored quote from local storage
     let calledQuote = localStorage.getItem('storedQuote');
     console.log("calledQuote = " + calledQuote);
-    
+
     // Check if there are stored quotes
     if (calledQuote) {
         // Parse the stored quotes
@@ -143,24 +157,23 @@ function cycleForward() {
             // Update the quote text and author
             quoteText.textContent = calledQuoteIndex[storedQuoteIndexNumber].text;
             authorText.textContent = calledQuoteIndex[storedQuoteIndexNumber].author;
-            
+
             quoteNumber();
         }
     }
 }
 
-function quoteNumber(){ 
-    arrayIndexNumber.textContent = storedQuoteIndexNumber+1;
+function quoteNumber() {
+    arrayIndexNumber.textContent = storedQuoteIndexNumber + 1;
 }
-
 
 //tweet quote 
 function tweetQuote() {
-//using backtick
-//tweet?text - is equivalent to query parameter = text, using the back ticks alows us to pass in a variable
-const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
-// "_blank", allows the window to open in a new tab
-window.open(twitterUrl, "_blank")
+    //using backtick
+    //tweet?text - is equivalent to query parameter = text, using the back ticks alows us to pass in a variable
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    // "_blank", allows the window to open in a new tab
+    window.open(twitterUrl, "_blank")
 }
 
 newQuoteButton.addEventListener("click", newQuote);
